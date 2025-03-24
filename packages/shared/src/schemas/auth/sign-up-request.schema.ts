9baaa95c2277d8795549cc2b037a20198012c90e
Ilannildo Viana <ilannildoviana12@gmail.com>
2025-03-24 16:56:02 -0300

    feat: setup describe

--- packages/shared/src/schemas/auth/sign-up-request.schema.ts
+++ packages/shared/src/schemas/auth/sign-up-request.schema.ts
@@ -3,14 +3,17 @@ import { z } from "zod";
 export const signUpRequestSchema = z.object({
   name: z
     .string({ required_error: "O nome é um campo obrigatório" })
-    .min(6, "O nome precisa ter no mínimo 6 letras."),
+    .min(6, "O nome precisa ter no mínimo 6 letras.")
+    .describe("Nome do usuário"),
   email: z
     .string({ required_error: "O e-mail é um campo obrigatório" })
-    .email("Informe um e-mail válido"),
+    .email("Informe um e-mail válido")
+    .describe("E-mail do usuário"),
   password: z
     .string({ required_error: "A senha é um campo obrigatório" })
     .min(6, "A senha precisa ter no mínimo 6 caracteres")
-    .max(32, "A senha precisa ter no máximo 32 caracteres"),
+    .max(32, "A senha precisa ter no máximo 32 caracteres")
+    .describe("Senha do usuário"),
 });
 
 export type ISignUpRequest = z.infer<typeof signUpRequestSchema>;
