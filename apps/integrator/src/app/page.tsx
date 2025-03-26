d52c81783c2bacc1402d8787f61514f5602be81d
Ilannildo Viana <ilannildoviana12@gmail.com>
2025-03-26 15:51:17 -0300

    feat: tailwindcss

--- apps/integrator/src/app/page.tsx
+++ apps/integrator/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@solarapp/ui/components/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-primary-500 text-white">
-      Hello World
+    <div className="flex items-center justify-center h-screen bg-chart-1 text-white">
+      <Button>Hello World</Button>
     </div>
   );
 }
