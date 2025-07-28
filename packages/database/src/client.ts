b4ff895c757a354e8898b47b12c32763e8ae04dd
Ilannildo Viana <ilannildoviana12@gmail.com>
2025-07-28 19:19:24 -0300

    setup: database instance

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -5,6 +5,9 @@ declare global {
 }
 
 export const prisma =
-  global.prisma || new PrismaClient();
+  global.prisma ||
+  new PrismaClient({
+    log: ["warn", "error"],
+  });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
