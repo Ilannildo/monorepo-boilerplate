97ad51748095d04d5cbec423972aeb14427e6277
Ilannildo Viana <ilannildoviana12@gmail.com>
2025-07-29 10:13:57 -0300

    setup: database

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from "../generated/prisma";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["warn", "error"],
-  });
+  globalForPrisma.prisma || new PrismaClient({ log: ["warn", "error"] });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
--- packages/database/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { prisma } from './client';
-export * from "../generated/prisma";
-//# sourceMappingURL=index.d.ts.map
